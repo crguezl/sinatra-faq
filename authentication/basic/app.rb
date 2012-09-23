6e999712f47e9cf3367190db9894410fd137babc
Casiano Rodriguez-Leon <casiano.rodriguez.leon@gmail.com>
2012-09-23 17:05:12 +0100

    shortener example operative

---
+++ authentication/basic/app.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'sinatra'
+
+use Rack::Auth::Basic, "Restricted Area" do |username, password|
+  [username, password] == ['admin', 'admin']
+end
+
+get '/' do
+  "You're welcome"
+end
+
+get '/foo' do
+  "You're also welcome"
+end
---
+++ mail/app.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+require 'pony'
+raise "provide the password" if ARGV.length.zero?
+get '/' do
+#    Pony.mail :to => 'susana.rodriguez.leon@gmail.com',
+#              :from => 'susana.rodriguez.leon@gmail.com',
+#              :subject => 'Howdy, Partna!',
+#              :body => 'Hello!'
+    email = 'casiano.rodriguez.leon@gmail.com'
+    pass = ARGV.shift
+    Pony.mail({
+      :to => email,
+      :body => "Hello Casiano",
+      :subject => 'Howdy, Partna!',
+      :via => :smtp,
+      :via_options => {
+          :address              => 'smtp.gmail.com',
+          :port                 => '587',
+          :enable_starttls_auto => true,
+          :user_name            => 'susana.rodriguez.leon@gmail.com',
+          :password             => pass,
+          :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+          :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+        }
+    })
+    "Check your email at #{email}"
+end
